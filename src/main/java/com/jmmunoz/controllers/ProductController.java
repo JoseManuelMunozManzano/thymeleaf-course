6ae733e2db7b8457adf8fa7e289d8b2e8862e648
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-12-25 07:26:21 +0100

    Fix getProductById

--- src/main/java/com/jmmunoz/controllers/ProductController.java
+++ src/main/java/com/jmmunoz/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.jmmunoz.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,7 +20,7 @@ public class ProductController {
     }
 
     @RequestMapping("/product/{id}")
-    public String getProductById(@RequestParam Integer id, Model model) {
+    public String getProductById(@PathVariable Integer id, Model model) {
         model.addAttribute("product", productService.getProduct(id));
 
         return "product";
