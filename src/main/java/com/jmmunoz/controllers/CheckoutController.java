0f2e6b0129da1668ac484e4d1c4393897dd8b0e5
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-12-25 16:24:36 +0100

    Validation - Setting Up Forms in Thymeleaf

--- src/main/java/com/jmmunoz/controllers/CheckoutController.java
+++ src/main/java/com/jmmunoz/controllers/CheckoutController.java
@@ -2,6 +2,7 @@ package com.jmmunoz.controllers;
 
 import com.jmmunoz.commands.CheckoutCommand;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,7 +13,9 @@ import javax.validation.Valid;
 public class CheckoutController {
 
     @RequestMapping("checkout")
-    public String checkoutForm() {
+    public String checkoutForm(Model model) {
+        model.addAttribute("checkoutCommand", new CheckoutCommand());
+
         return "checkoutform";
     }
 
